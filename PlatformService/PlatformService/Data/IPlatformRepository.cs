9f09d0198ec5dffa4fe931ad1a24a8e8e8c8991f
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-01-09 21:59:38 +0300

    Add model and dbcontext

---
+++ PlatformService/PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,15 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepository
+{
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetAllPlatforms();
+
+    Platform GetPlatformById(long id);
+
+    void CreatePlatform(Platform platform); 
+
+}
