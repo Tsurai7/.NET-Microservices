8a10165d19ef0dd7470bb582b4750b41c30affcd
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-01-10 10:54:27 +0300

    Add docker support

--- PlatformService/PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
@@ -23,10 +23,34 @@ public class PlatformsController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
     {
-        Console.WriteLine("--> Getting Platforms...");
-
         var platformItem = _repository.GetAllPlatforms();
 
         return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(long id)
+    {
+        var platformItem = _repository.GetPlatformById(id);
+
+        if (platformItem != null)
+        {
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+
+        return NotFound();
+    }
+    
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    {
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+        
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { id = platformReadDto.Id}, platformReadDto);
     }
 }
